0f76055d3387a9cf167eacb55c9ff960d87dc035
JuanPabloLino <juanlino261193@gmail.com>
2021-08-11 17:59:17 -0300

    segunda entrega

---
+++ src/components/navBar.js
@@ -0,0 +1,19 @@
+const NavBar = () => {
+    return(<nav class="navbar navbar-expand-lg navbar-dark bg-dark static-top">
+    <div class="container-fluid">
+        <nav class="navbar navbar-inverse">
+            <div class="container-fluid">
+            <ul class="nav navbar-nav">
+                <li class="nav-link" ><a class="nav-link" href="#">Home</a></li>
+                <li class="nav-link" ><a class="nav-link" href="#">Nuestros productos</a></li>
+                <li class="nav-link" ><a class="nav-link" href="#">Nuestras ofertas</a></li>
+                <li class="nav-link" ><a class="nav-link" href="#">Nosotros</a></li>
+            </ul>
+            </div>
+        </nav>
+    </div>
+</nav>
+    )}
+
+
+export default NavBar;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+/* Solo se aplica una vez durante el proyecto, al principio de la aplicacion. */
+/* Levanta o dibuja el inicio de la aplicacion. */
+/* Cosas que va a tener el index: 4 basicas;
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+1) La variable React en scope; seria cuando pongo el script al final del html;
+2) Tener la variable ReactDOM en scope, otra libreria adicional. (para dibujar la aplicacion en el DOM);
+3) Tener un elemento de React componente.(Nuestra aplicacion);
+4) Tener una funcion Render corriendo.(Renderizar nuestra funcion);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+Esto lo vamos a hacer mediante Imports;
+*/
+
+
+//1
+import React from "react"
+
+//2
+import ReactDOM from "react-dom"
+//3 Importo el componente de App.js. La forma que se debe escrbirir es:
+import App from "./App"
+
+import "./estilos.css"
+//4
+ReactDOM.render(<App/>,document.querySelector("#root"));
